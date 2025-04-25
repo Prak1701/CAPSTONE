192ab2c1e8956986e4bfb4ad087714421f53b510
Prak1701 <prakriti6negi@gmail.com>
2025-04-25 16:52:46 +0530

    UI

--- phishing-backend/routes/phishingRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { analyzePhishing } = require("../controllers/phishingController");
-const authMiddleware = require("../middleware/authMiddleware");
-
-router.post("/analyze", authMiddleware, analyzePhishing);
-
-module.exports = router;
--- user interface/src/views/plugin.js
+++ user interface/src/views/plugin.js
@@ -9,7 +9,7 @@ const Plugin = () => {
         Stay one step ahead of phishing attacks. Download our Firefox plugin and keep your emails safe, always.
       </p>
       <a
-        href="/downloads/phishing-checker-firefox.xpi"
+        href="/phishing-detector-classified.zip"
         download
         className="plugin-download-btn"
       >
